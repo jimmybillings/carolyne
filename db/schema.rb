2ca582d575d7c9df399f739615ee654f8fa226d0
James Billings <james_billings@me.com>
2015-07-20 11:16:21 -0600

    switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150211030545) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blocks", force: true do |t|
     t.string   "page"
     t.string   "usecase"
